170c3e328c9ff3eb7ae26ce7406f245994f30da9
mkounkel <mkounkel@umich.edu>
2023-03-16 13:34:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="apogeesb2",
-    version="1.1",
+    version="1.2",
     author="Marina Kounkel",
     author_email="marina.kounkel@wwu.edu",
     description="Examine CCFs in the APOGEE apstar files to search for double lined spectroscopic binaries",
